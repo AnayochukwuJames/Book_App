ad6eb28fa64cd3a8444be3053841efec6bbb3638
Anayochukwu James <anayochukwujames11@gmail.com>
2024-09-06 15:01:26 +0100

    Update initial commit

---
+++ src/main/java/com/james/book_app/role/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.james.book_app.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String roleStudent);
+}
